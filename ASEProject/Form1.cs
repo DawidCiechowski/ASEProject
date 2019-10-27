f7f3ebd5a833eb4ade1a9eec9b110fcbbca8ca4d
DawidCiechowski <dawid.ciechowski@gmail.com>
2019-10-27 11:23:32 +0000

    First basic commands run

--- ASEProject/Form1.cs
+++ ASEProject/Form1.cs
@@ -9,27 +9,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ASEProject
 {
     public partial class Form1 : Form
     {
 
         Bitmap bm;
+        int currentX;
+        int currentY;
         public Form1()
         {
             InitializeComponent();
+            //Initialize bitmap
             bm = new Bitmap(pbMainDraw.Size.Width, pbMainDraw.Size.Height);
             pbMainDraw.Image = bm;
+
+            //Initialize coordinates
+
+            currentX = 0;
+            currentY = 0;
         }
 
         private void textBoxCommand_KeyUp(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter)
             {
-                if (textBoxCommand.Text.ToString().Equals("run"))
+                string com = textBoxCommand.Text.ToString();
+                if (com.Equals("run"))
                 {
-
-                    drawCircle();
 
                     string[] commands = richTextBoxCommands.Text.ToString().Split('\n');
 
@@ -37,12 +45,18 @@ namespace ASEProject
                     {
                         Debug.WriteLine(command);
                     }
-                } else if(textBoxCommand.Text.ToString().Equals("clearText"))
+                } else if(com.Equals("clearText"))
                 {
                     richTextBoxCommands.Clear();
-                } else if(textBoxCommand.Text.ToString().Equals("clear"))
+                } else if(com.Equals("clear"))
                 {
                     clearCommand();
+                } else if(com.Contains("circle(") && com.EndsWith(")"))
+                {
+                    if(validateInput("circle", com))
+                    {
+                        drawCircle(int.Parse(com.Substring(7, com.Length - 8)));
+                    }
                 }
 
                 textBoxCommand.Clear();
@@ -52,9 +66,7 @@ namespace ASEProject
         private void Form1_Load(object sender, EventArgs e)
         {
             //Make image background white on Load
-            //clearCommand();
-            
-            drawCircle();
+            clearCommand();
         }
 
         public void clearCommand()
@@ -68,15 +80,30 @@ namespace ASEProject
             pbMainDraw.Invalidate();
         }
 
-        public void drawCircle()
+        public void drawCircle(int radius)
         {
             using(Graphics g = Graphics.FromImage(bm))
             {
-                g.DrawEllipse(new Pen(Color.Red), 100, 100, 30, 30);
+                g.DrawEllipse(new Pen(Color.Red), currentX, currentY, radius, radius);
                 g.Dispose();
             }
 
             pbMainDraw.Invalidate();
+
+            string f = "(LL)";
+            string[] s = f.Split(); 
+        }
+
+        public bool validateInput(string type, string input)
+        {
+            if(type.Equals("circle"))
+            {
+
+                int a;
+                return int.TryParse(input.Substring(7, input.Length-8), out a);
+            }
+
+            return false;
         }
     }
 }
