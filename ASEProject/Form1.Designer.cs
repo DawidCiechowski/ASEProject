576d53baf91f3c6154b8c10545c24004704d591d
DawidCiechowski <dawid.ciechowski@gmail.com>
2019-10-27 01:16:22 +0100

    Added the very basics

---
+++ ASEProject/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ASEProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxCommands = new System.Windows.Forms.RichTextBox();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.pbMainDraw = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxCommands
+            // 
+            this.richTextBoxCommands.Location = new System.Drawing.Point(13, 13);
+            this.richTextBoxCommands.Name = "richTextBoxCommands";
+            this.richTextBoxCommands.Size = new System.Drawing.Size(607, 433);
+            this.richTextBoxCommands.TabIndex = 0;
+            this.richTextBoxCommands.Text = "";
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(13, 485);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(607, 22);
+            this.textBoxCommand.TabIndex = 1;
+            this.textBoxCommand.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxCommand_KeyUp);
+            // 
+            // pbMainDraw
+            // 
+            this.pbMainDraw.Location = new System.Drawing.Point(650, 13);
+            this.pbMainDraw.Name = "pbMainDraw";
+            this.pbMainDraw.Size = new System.Drawing.Size(630, 493);
+            this.pbMainDraw.TabIndex = 2;
+            this.pbMainDraw.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 555);
+            this.Controls.Add(this.pbMainDraw);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.richTextBoxCommands);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainDraw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxCommands;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.PictureBox pbMainDraw;
+    }
+}
+
---
+++ ASEProject/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASEProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxCommand_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (textBoxCommand.Text.ToString().Equals("run()"))
+                {
+                    string[] commands = richTextBoxCommands.Text.ToString().Split('\n');
+
+                    foreach (string command in commands)
+                    {
+                        Debug.WriteLine(command);
+                    }
+                } else if(textBoxCommand.Text.ToString().Equals("clear()"))
+                {
+                    richTextBoxCommands.Clear();
+                }
+
+                textBoxCommand.Clear();
+            }
+        }
+    }
+}
